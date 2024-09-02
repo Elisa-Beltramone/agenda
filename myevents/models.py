716cfe27c6500d18e7eee53b0973d706b10b075f
Elisa <elibeltramone@gmail.com>
2024-09-02 11:27:00 +0200

    updates

--- myevents/models.py
+++ myevents/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Venue(models.Model):
+    name_venue = models.CharField('Venue name', max_length=120)
+    address = models.CharField(max_length=200)
+    zip_code = models.CharField(max_length=15)
+    phone = models.CharField('Contact Phone', max_length=12)
+
+    def __str__(self):
+        return self.name_venue
+    
+class AgendaUser(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    email = models.EmailField('User email')
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+class Event(models.Model):
+    name_event = models.CharField('Event name', max_length=120)
+    date_event = models.DateTimeField('Event date')
+    venue_event = models.ForeignKey(Venue, blank=True, null=True, on_delete=models.CASCADE)
+    manager_event = models.CharField(max_length=60)
+    description = models.TextField(blank=True)
+    attendees = models.ManyToManyField(AgendaUser, blank=True)
+
+    def __str__(self):
+        return self.name_event
--- myevents/views.py
+++ myevents/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 import calendar
 from calendar import HTMLCalendar
+from datetime import datetime
 
-def home(request, year, month):
+def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
+    month = month.capitalize()
+    month_number = list(calendar.month_name).index(month)
+    cal = HTMLCalendar().formatmonth(year, month_number)
     return render(request, 'home.html', {
         "year": year,
         "month": month,
+        "cal": cal,
     })
